06c13c8b52b2094515c495c23d9c93fa366bf8a1
Dan F-M <danfm@nyu.edu>
2012-03-11 15:27:21 -0400

    checking command line message

--- gc.py
+++ gc.py
@@ -6,8 +6,14 @@ Wrapper around git commit to keep track of stuff.
 
 import sys
 import subprocess
+import re
 
-cmd = "git commit "+" ".join(sys.argv[1:])
+args = " ".join([a if " " not in a else "\""+a+"\"" for a in sys.argv[1:]])
+if re.search("\-[a-zA-Z]*m", args) is None:
+    print "You need to use the command line message for now... sorry!"
+    sys.exit(1)
+cmd = "git commit "+args
+print cmd
 
 proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
 ret  = proc.wait()
@@ -16,4 +22,5 @@ if ret == 0:
     print proc.communicate()[0]
 else:
     print proc.communicate()[0]
+    sys.exit(ret)
 
