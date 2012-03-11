45aa457584d8688692c2b941e33769c0f6d8ae8c
Dan F-M <danfm@nyu.edu>
2012-03-11 15:57:05 -0400

    adding message

--- gc.py
+++ gc.py
@@ -44,8 +44,8 @@ if ret != 0:
 
 # Parse return values
 o = proc.communicate()[0]
-g = re.search("\[.*?\] (.*?) (.*?) files changed, (.*?) insertions\(\+\),"\
-    +"(.*?) deletions\(\-\)", o, flags=re.M|re.S).groups()
+g = re.search("\[.*?\] (.*) ([0-9]*?) files changed, ([0-9]*?) insertions\(\+\),"\
+    +"([0-9]*?) deletions\(\-\)", o, flags=re.M|re.S).groups()
 print o
 
 # Get the remote info
