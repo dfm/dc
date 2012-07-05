9a0537e4a2f7d279a17e01ec4a6546605f362879
Dan F-M <danfm@nyu.edu>
2012-07-06 00:40:59 +0200

    track the app too

--- keys/keys.m
+++ keys/keys.m
@@ -10,7 +10,17 @@
     [NSEvent addGlobalMonitorForEventsMatchingMask:NSKeyDownMask
                                            handler:^(NSEvent *event){
         NSDate *now = [[NSDate alloc] init];
-        NSString *update = [NSString stringWithFormat: @"%@ %d %@\n", [now description], event.keyCode, event.charactersIgnoringModifiers];
+
+        // Find the currently running app.
+        NSArray *runningApps = [[NSWorkspace sharedWorkspace] runningApplications];
+        NSString *appName = @"unknown";
+        for (int i = 0; i < [runningApps count]; i++) {
+            NSRunningApplication *app = [runningApps objectAtIndex: i];
+            if ([app isActive])
+                appName = [app localizedName];
+        }
+
+        NSString *update = [NSString stringWithFormat: @"%@ %@ %d %@\n", [now description], appName, event.keyCode, event.charactersIgnoringModifiers];
         FILE *f = fopen([[NSHomeDirectory() stringByAppendingString: @"/.keys"] UTF8String], "a");
         fprintf(f, "%s", [update UTF8String]);
         fclose(f);
@@ -24,10 +34,10 @@ int main(int argc, char *argv[])
     AppDelegate *delegate = [[AppDelegate alloc] init];
     NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
     NSApplication *app = [NSApplication sharedApplication];
-    
+
     [app setDelegate:delegate];
     [NSApp run];
-    
+
     [pool drain];
     [delegate release];
     return 0;
